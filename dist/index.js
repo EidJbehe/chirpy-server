b078e66cd8ba2f27edcef8ab013b4b4017816fc2
eidjbehe <eidjbehe@gmail.com>
2026-03-08 02:48:16 +0200

    Complete custom handler and readiness endpoint lesson

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+const handlerReadiness = (req, res) => {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+};
+app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
