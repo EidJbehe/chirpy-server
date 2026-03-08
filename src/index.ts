c9b83afc840f0a5d3ced07750c25168eecf923a6
eidjbehe <eidjbehe@gmail.com>
2026-03-08 04:11:23 +0200

    Add chirp validation endpoint with JSON responses

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { config } from "./config.js";
 
 const app = express();
 const PORT = 8080;
-
+app.use(express.json());
 const middlewareLogResponses = (
   req: Request,
   res: Response,
@@ -53,7 +53,30 @@ const handlerReset = (_req: Request, res: Response): void => {
   res.set("Content-Type", "text/plain; charset=utf-8");
   res.send("Hits reset to 0");
 };
+const handlerValidateChirp = (req: Request, res: Response): void => {
+  type RequestBody = {
+    body: string;
+  };
 
+  const params: RequestBody = req.body;
+
+  if (params.body.length > 140) {
+    res.set("Content-Type", "application/json");
+    res.status(400).send(
+      JSON.stringify({
+        error: "Chirp is too long",
+      })
+    );
+    return;
+  }
+
+  res.set("Content-Type", "application/json");
+  res.status(200).send(
+    JSON.stringify({
+      valid: true,
+    })
+  );
+};
 app.use(middlewareLogResponses);
 
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
@@ -61,7 +84,7 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
-
+app.post("/api/validate_chirp", handlerValidateChirp);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
