5c56b44de4446e9d6b2f91939dbeae906967edfc
eidjbehe <eidjbehe@gmail.com>
2026-03-08 17:24:50 +0200

    Add database config and user creation query

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,12 @@
+process.loadEnvFile();
+function envOrThrow(key) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+    return value;
+}
 export const config = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
